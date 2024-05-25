0a76fd9608a6cdacbecbfb52db460ee0a786e7e6
Johnni5 <johni5@proton.me>
2024-05-25 16:48:22 +0200

    final at verison 1.0 incl sounds

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
 let currentQuestion = 19;
+let countCorrectAnswers = 0;
+
+let AUDIO_SUCCESS = new Audio();
+AUDIO_SUCCESS.src = "./audio/correct.mp3";
+
+let AUDIO_FAIL = new Audio();
+AUDIO_FAIL.src = "./audio/wrong.mp3";
 
 function render() {
   const actualQuestion = document.getElementById('counter-actual');
@@ -9,6 +16,7 @@ function render() {
 
   showQuestion();
 }
+
 
 function showQuestion() {
   let question = document.getElementById('question');
@@ -27,19 +35,49 @@ function showQuestion() {
   answerD.innerHTML = quizHtml[currentQuestion]['answer_4'];
 }
 
+
+function answer(answer) {
+  let editedAnswer = answer.replace('answer_', '');
+  let rightNumber = quizHtml[currentQuestion]['correctAnswer'];
+  let rightAnswer = 'answer_' + rightNumber;
+
+  if (editedAnswer == quizHtml[currentQuestion]['correctAnswer']) {
+    AUDIO_SUCCESS.play();
+    countCorrectAnswers++;
+    const answerCard = document.getElementById(answer);
+    answerCard.parentNode.classList.add('bg-success');
+  } else {
+    AUDIO_FAIL.play();
+    const answerCard = document.getElementById(answer);
+    answerCard.parentNode.classList.add('bg-danger');
+    document.getElementById(rightAnswer).parentNode.classList.add('bg-success');
+  }
+  document.getElementById('btn-next-question').disabled = false;
+}
+
 function nextQuestion() {
   document.getElementById('btn-next-question').disabled = true;
 
   delChoices();
 
-  currentQuestion++;
+  currentQuestion++; 
 
-  render();
-
-  // if (currentQuestion == quizHtml.length) {
-  //   TODO show end-screen
-  //   alert('You did it!');
-  // }
+  if (currentQuestion >= quizHtml.length) {
+    document.getElementById('quiz-ende').classList.remove('d-none');
+    document.getElementById('quiz-start').classList.add('d-none');
+    document.getElementById('logo').classList.add('d-none');
+    document.getElementById('score').innerHTML = countCorrectAnswers;
+    document.getElementById('q-length').innerHTML = quizHtml.length;
+    //document.getElementById('progress-bar').style = '100%;';
+    //document.getElementById('progress-bar').innerHTML = '100%';
+  } else {
+    let percentQuestion = (currentQuestion+1) / quizHtml.length;
+    percentQuestion = Math.round(percentQuestion*100);  
+    document.getElementById('progress-bar').style.width=percentQuestion + '%';
+    document.getElementById('progress-bar').innerHTML = percentQuestion + '%';
+    render();
+    showQuestion();
+  }
 }
 
 function delChoices() {
@@ -54,24 +92,22 @@ function delChoices() {
     .parentNode.classList.remove('bg-success', 'bg-danger');
   document
     .getElementById('answer_4')
-    .parentNode.classList.remove('bg-success', 'bg-danger');
+   .parentNode.classList.remove('bg-success', 'bg-danger');
 }
 
-let countCorrectAnswers = 0;
+function restartGame() {
+  document.getElementById('quiz-ende').classList.add('d-none');
+  document.getElementById('quiz-start').classList.remove('d-none');
+  document.getElementById('logo').classList.remove('d-none');
+  
+  resetGame();
+  render();
 
-function answer(answer) {
-  let editedAnswer = answer.replace('answer_', '');
-  let rightNumber = quizHtml[currentQuestion]['correctAnswer'];
-  let rightAnswer = 'answer_' + rightNumber;
+}
 
-  if (editedAnswer == quizHtml[currentQuestion]['correctAnswer']) {
-    countCorrectAnswers++;
-    const answerCard = document.getElementById(answer);
-    answerCard.parentNode.classList.add('bg-success');
-  } else {
-    const answerCard = document.getElementById(answer);
-    answerCard.parentNode.classList.add('bg-danger');
-    document.getElementById(rightAnswer).parentNode.classList.add('bg-success');
-  }
-  document.getElementById('btn-next-question').disabled = false;
+function resetGame() {
+  currentQuestion = 0;
+  countCorrectAnswers = 0;
+  document.getElementById('progress-bar').style = '0%;';
+  document.getElementById('progress-bar').innerHTML = '0%';
 }
